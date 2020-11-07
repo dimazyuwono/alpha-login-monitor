a859f383ebc510d857d9e8a598a2fa39b666e0f9
Amal Syahreza <amal.syahreza@gmail.com>
2020-11-08 00:10:22 +0700

    initialize logstream proto

---
+++ logstream/generate.sh
@@ -0,0 +1,12 @@
+set -eu
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+cd $DIR
+
+protoc \
+    --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    logstream.proto
